b61c706700a0ba07ab73a1cc3d864da8878d98ee
Ahmed Daoud <Ahmed.Daoud@Khazyn.app>
2025-06-25 13:01:33 +0300

    Added Version To The Scribt

---
+++ Day 1 _s 1-8.py
@@ -0,0 +1,249 @@
+#-----------------------------------------------------------------------------------------
+print("********************* start of Day 1_s1-8 **************************")
+#-----------------------------------------------------------------------------------------
+print("********************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 1 **************************")
+#---------------------------------------------------------------------------------------#-----                
+
+#
+    # ------------ section 1 ----------------
+    #             Introduction          
+    # python is a high level programming language
+    # python is an interpreted language
+    # python is an object oriented programming language
+    # python is a dynamically typed language
+    # python is a multi-paradigm programming language
+    # python is a general purpose programming language
+    # python is a cross platform programming language
+    # python is a portable programming language
+    # python is a powerful programming language
+    # python is a flexible programming language
+    # python is a scalable programming language
+    # python is a secure programming language
+    # python is a reliable programming language
+    # python is a robust programming language
+    # python is a scalable programming language 
+    # python is a flexible programming language
+    # python is a extensible programming language
+    # python is a maintainable programming language
+    # python is a readable programming language
+    # python is a understandable programming language
+    # python is a reusable programming language 
+
+#-----------------------------------------------------------------------------------------
+print("********************** end of section 1 ****************************")
+#-----------------------------------------------------------------------------------------
+print("********************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 2 **************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 2 ----------------
+    #            what I need to learn
+    # I need to learn the following topics:
+    # 1. Introduction to python     
+    # 2. Installation of python
+    # 3. Python IDEs    
+    # 4. Python syntax
+    # 5. Python comments
+    # 6. Python data types
+    # 7. Python variables
+    # 8. Python operators
+    # 9. Python control flow
+    # 10. Python functions
+    # 11. Python modules
+    # 12. Python packages
+    # 13. Python libraries
+    # 14. Python computer vision
+    # 15. Python cloud
+
+#-----------------------------------------------------------------------------------------
+print("********************** end of section 2 ****************************")       
+#-----------------------------------------------------------------------------------------
+print("********************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 3 **************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 3 ----------------
+    #             print function
+    # print function is used to display output on the screen
+    # print function can take multiple arguments
+    # print function can take multiple data types
+    # print function can take multiple variables
+    # print function can take multiple expressions
+    # print function can take multiple strings
+    # print function can take multiple numbers
+    # print function can take multiple lists
+    # print function can take multiple tuples
+    # print function can take multiple dictionaries
+print("Hello World!")          # prints a string
+print(10)                   # prints an integer
+print(10.5)                # prints a float
+print("My name is", "danu")  # prints multiple strings
+print("My name is", "Daoud", "and I am", 30, "years old")  # prints multiple strings and an integer
+print("My name is", "Daoud", "and I am", 30, "years old", sep=" - ")  # prints multiple strings and an integer with a separator
+print("My name is", "Daoud", "and I am", 30, "years old", end="!")  # prints multiple strings and an integer with an end character
+print("My name is", "Daoud", "and I am", 30, "years old", sep=" - ", end="!")  # prints multiple strings and an integer with a separator and an end character
+print("My name is", "Daoud", "and I am", 30, "years old", sep=" - ", end="!\n")  # prints multiple strings and an integer with a separator and an end character with a newline
+print("My name is", "Daoud", "and I am", 30, "years old", sep=" - ", end="!\n\n")  # prints multiple strings and an integer with a separator and an end character with two newlines
+print("My name is", "Daoud", "and I am", 30, "years old", sep=" - ", end="!\n\n\n")  # prints multiple strings and an integer with a separator and an end character with three newline
+
+#-----------------------------------------------------------------------------------------
+print("*********************** end of section 3 ****************************")
+#-----------------------------------------------------------------------------------------
+print("********************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 4 **************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 4 ----------------
+    #             comments
+    # comments are used to explain the code 
+    # comments are ignored by the interpreter
+    # comments are used to make the code more readable
+    # comments are used to make the code more understandable
+    # comments are used to make the code more maintainable
+    # comments are used to make the code more efficient
+    # comments are used to make the code more reusable
+    # comments are used to make the code more portable  
+    # comments are used to make the code more secure
+    # comments are used to make the code more reliable
+    # comments are used to make the code more robust
+    # comments are used to make the code more scalable
+    # comments are used to make the code more flexible
+    # comments are used to make the code more extensible
+    # comments are used to make the code more maintainable
+    # comments are used to make the code more readable
+print("# This is a comment")  # this is a comment
+print("This is not a comment")  # this is not a comment
+print("n=enkk")
+
+#-----------------------------------------------------------------------------------------
+print("************************** end of section 4 ****************************")
+#-----------------------------------------------------------------------------------------
+print("************************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 5 ******************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 5 ----------------
+    #           Dealing with data in
+    #           python
+    # data is stored in variables
+    # data is processed by the interpreter
+    # data is displayed on the screen
+    # data is stored in memory
+    # data is stored in files
+    # data is stored in databases
+    # data is stored in the cloud
+    # data is stored in the internet
+    # data is stored in the web
+    # data is stored in the network
+    # data is stored in the system  
+    # data is stored in the computer
+    # data is stored in the device
+    # data is stored in the application
+    # data is stored in the program
+    # data is stored in the software
+    # data is stored in the hardware
+
+#-----------------------------------------------------------------------------------------
+print("************************* end of section 5 ****************************")
+#-----------------------------------------------------------------------------------------
+print("************************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 6 **************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 6 ----------------
+    #               Data Types
+    # Data types are used to define the type of data that can be stored in a variable
+    # Python has several built-in data types        
+    #------------------------------------------------
+    # 1. int    : used to store integers
+    # 2. float  : used to store floating point numbers        
+    # 3. str    : used to store strings
+    # 4. list   : used to store a collection of items
+    # 5. tuple  : used to store a collection of items that can not be changed
+    # 6. dict   : used to store a collection of key-value pairs
+    # 7. bool   : used to store a boolean value (True or False)
+    # 8. set    : used to store a collection of unique items
+    # 9. None   : used to represent a null value       
+    #------------------------------------------------
+
+print(type(10))                 #<class 'integer'>
+print(type(-10.2))              #<class 'float'>
+print(type("daoud"))            #<class 'str'>
+print(type ([1,2,3]))           #<class 'list'>
+print(type ((1,2,3)))           #<class 'tuple'>
+print(type({"name":"daoud"}))   #<class 'dict'>
+print(type(2==5))               #<class 'bool'>
+
+#-----------------------------------------------------------------------------------------
+print("************************* end of section 6 ****************************")
+#-----------------------------------------------------------------------------------------
+print("************************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 7 **************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 7 ----------------
+    #             variables
+    # variables are used to store data
+    # variables are created when you assign a value to them
+    # variable names can contain letters, numbers, and underscores 
+    # variables can be assigned to any data type
+    # variables can be reassigned to any data type
+    # variable names must follow these rules:
+    #------------------------------------------------
+    # can not start with a number
+    # can not contain special characters
+    # can not contain spaces 
+    # can not use ketwords
+    #------------------------------------------------
+my_name ="ahmed daoud" 
+print(my_name)
+
+#-----------------------------------------------------------------------------------------
+print("************************* end of section 7 ****************************")
+#-----------------------------------------------------------------------------------------
+print("************************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** start of section 8 **************************")
+#-----------------------------------------------------------------------------------------
+
+    # ------------ section 8 ----------------
+    #             variables continued
+    # source code is a file that contains python code
+    # translation is the process of converting source code into machine code
+    # compilation is the process of converting source code into machine code
+    # interpretation is the process of executing source code line by line
+    # execution is the process of running the machine code
+    # runtime is the time when the program is running
+    # -------------------------------------------------
+    # reserved words are words that are used by python
+    # reserved words can not be used as variable names
+    # reserved words are also called keywords
+help('keywords') # to see the list of keywords
+
+#-----------------------------------------------------------------------------------------
+print("************************* end of section 8 ****************************")
+#-----------------------------------------------------------------------------------------
+print("************************************************************************")
+#-----------------------------------------------------------------------------------------
+print("********************** end of Day 1_s1-8 ****************************")
+#-----------------------------------------------------------------------------------------
+
+# This code is a simple introduction to Python programming, covering basic concepts such as data types, variables, comments, and the print function.
+# It also includes a brief overview of Python's features and capabilities, such as being a high-level, interpreted, object-oriented, and dynamically typed language.
+# The code demonstrates how to use the print function to display different types of data, how to create and use variables, and how to work with comments.
+# The code also provides a list of topics to learn in Python, including installation, syntax, control flow, functions, modules, packages, libraries, computer vision, and cloud computing.
+# The code is structured into sections, each with a specific focus, and includes explanations of the concepts covered.
+# The code is designed to be easy to understand and follow, making it suitable for beginners who are just starting to learn Python programming.
+# The code is also designed to be modular, allowing for easy expansion and modification as needed.
+# The code is a good starting point for anyone interested in learning Python programming and can be used as a reference for future projects.    
+# The code is also a good example of how to structure Python code for readability and maintainability.
+# The code is a simple introduction to Python programming, covering basic concepts such as data types, variables, comments, and the print function. 
+# It also includes a brief overview of Python's features and capabilities, such as being a high-level, interpreted, object-oriented, and dynamically typed language.
